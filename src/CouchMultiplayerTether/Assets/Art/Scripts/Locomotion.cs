d325ee2dc86e6c784f2d23c93af27b03bc727297
Sloan Kelly <sloankelly@gmail.com>
2018-06-24 17:43:09 -0400

    Completed source from the video. Contains fix for player's speed not being clamped when moving left.

--- src/CouchMultiplayerTether/Assets/Art/Scripts/Locomotion.cs
+++ src/CouchMultiplayerTether/Assets/Art/Scripts/Locomotion.cs
@@ -3,7 +3,10 @@
 public class Locomotion : MonoBehaviour
 {
     Rigidbody2D _rb;
+    Tether _tether;
     float speed = 15f;
+
+    float maxSpeed = 5f;
 
     public KeyCode left = KeyCode.A;
     public KeyCode right = KeyCode.D;
@@ -11,6 +14,7 @@ public class Locomotion : MonoBehaviour
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _tether = GetComponent<Tether>();
     }
 
     private void Update()
@@ -26,9 +30,29 @@ public class Locomotion : MonoBehaviour
             tmpSpeed = Vector2.right * Time.deltaTime * speed;
         }
 
-        if (tmpSpeed != Vector2.zero)
+        if (tmpSpeed != Vector2.zero && MovementAllowed(tmpSpeed))
         {
             _rb.AddForce(tmpSpeed, ForceMode2D.Impulse);
+
+            // This checks that the player is not above the max speed
+            if (Mathf.Abs(_rb.velocity.x) > maxSpeed)
+            {
+                var tmp = _rb.velocity;
+                // Clamp the player to the maximum speed allowed
+                // Taking care to ensure that the speed is in the right
+                // direction; -1 is left, 1 is right
+                tmp.x = Mathf.Sign(_rb.velocity.x) * maxSpeed;
+                _rb.velocity = tmp;
+            }
         }
+    }
+
+    private bool MovementAllowed(Vector3 speed)
+    {
+        if (_tether.IsCloser(speed * 5f + transform.position))
+            return true;
+
+        _rb.velocity = Vector2.zero;
+        return false;
     }
 }
---
+++ src/CouchMultiplayerTether/Assets/Art/Scripts/Tether.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Tether : MonoBehaviour
+{
+    public Transform other;
+
+    public float maxDistance = 20f;
+
+    public bool AtMax { get; private set; }
+
+    public float Distance
+    {
+        get
+        {
+            var distance = (other.position - transform.position).magnitude;
+            return distance / maxDistance;
+        }
+    }
+
+    public bool IsCloser(Vector3 pos)
+    {
+        var distance = (other.position - pos).magnitude;
+        return distance < maxDistance;
+    }
+
+    private void Update()
+    {
+        var distance = (other.position - transform.position).magnitude;
+        AtMax = distance >= maxDistance;
+    }
+}
