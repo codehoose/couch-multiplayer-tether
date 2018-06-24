2fe7735d02862c79dcfeb75feb394f55aefb8694
Sloan Kelly <sloankelly@gmail.com>
2018-06-24 13:15:28 -0400

    Starter code for couch-tether

---
+++ src/CouchMultiplayerTether/Assets/Art/Scripts/CameraFollow.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    Camera _cam;
+
+    public GameObject[] targets;
+
+    float speed = 10f;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        _cam = GetComponent<Camera>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (targets == null || targets.Length == 0)
+            return;
+
+        var tmp = Vector3.zero;
+        for (int i = 0; i < targets.Length; i++)
+        {
+            tmp += targets[i].transform.position;
+        }
+
+        tmp /= targets.Length;
+
+        if (tmp.x < 0)
+            tmp.x = 0;
+
+        tmp.y = _cam.transform.position.y;
+        tmp.z = _cam.transform.position.z;
+
+        _cam.transform.position = Vector3.Lerp(_cam.transform.position, 
+                                               tmp, 
+                                               Time.deltaTime);
+    }
+}
---
+++ src/CouchMultiplayerTether/Assets/Art/Scripts/Locomotion.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Locomotion : MonoBehaviour
+{
+    Rigidbody2D _rb;
+    float speed = 15f;
+
+    public KeyCode left = KeyCode.A;
+    public KeyCode right = KeyCode.D;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        var tmpSpeed = Vector2.zero;
+
+        if (Input.GetKey(left))
+        {
+            tmpSpeed = Vector2.left * Time.deltaTime * speed;
+        }
+        else if (Input.GetKey(right))
+        {
+            tmpSpeed = Vector2.right * Time.deltaTime * speed;
+        }
+
+        if (tmpSpeed != Vector2.zero)
+        {
+            _rb.AddForce(tmpSpeed, ForceMode2D.Impulse);
+        }
+    }
+}
